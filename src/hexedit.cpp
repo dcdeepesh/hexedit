edccf327ffe5514ebf7f73cc8780c6387349709b
Deepesh Choudhary <dcdeepesh7@gmail.com>
2020-08-29 18:46:33 +0530

    Integrate cmdline args and improve help messages

--- src/hexedit.cpp
+++ src/hexedit.cpp
@@ -1,3 +1,5 @@
+#include "ArgParser/ArgParser.h"
+#include "ArgParser/ArgParseException.h"
 #include "Buffer.h"
 #include "Global.h"
 #include "Marker.h"
@@ -10,21 +12,51 @@
 #include <iostream>
 #include <string>
 
-void printUsage() {
-    std::cout <<
-        "No file provided.\n"
-        "Usage: ./hexedit <fileName>\n"
-        "Use option '-h' or --help' to get complete help."
-    << std::endl;
-}
+std::string HELP_MSG =
+    "Hexedit version 1.0\n"
+    "A TUI hex editor\n\n"
+    "Usage: hexedit [OPTIONS] <file>\n"
+    "Options:\n"
+    "  -h, --help         Print this help message\n"
+    "  -s, --std-colors   Use standard colors, instead of default terminal colors\n"
+    "                     Enable this if there are issues with default colors.\n"
+    "  -c <cols>,         Set the number of columns. Recommended: 4, 8, 16, 32.\n"
+    "    --columns=<cols> There are no restrictions on this number, but columns\n"
+    "                     wider than screen can misbehave.\n\n"
+    "Note: If you're having color issues (e.g. when your terminal uses light mode),\n"
+    "      use option '-s' or '--std-colors'";
+
+std::string SHORT_HELP_MSG =
+    "No file provided.\n"
+    "Usage: hexedit [OPTIONS] <file>\n"
+    "Use option '-h' or --help' to get complete help.";
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        printUsage();
+        std::cout << SHORT_HELP_MSG << std::endl;
+        return 1;
+    }
+    
+    ArgParser ap(argc, argv, "hs", "c");
+    try {
+        ap.parse();
+    } catch (ArgParseException e) {
+        std::cout << e.what() << std::endl;
+        return 2;
+    }
+
+    if (ap.isSwitchSet("h") || ap.isSwitchSet("help")) {
+        std::cout << HELP_MSG << std::endl;
         return 0;
     }
 
-    std::string fileName = argv[1];
+    auto operands = ap.getOperands();
+    if (operands.size() == 0) {
+        std::cout << SHORT_HELP_MSG << std::endl;
+        return 1;
+    }
+
+    std::string fileName = operands[0];
     
     initscr();
     G::height = LINES;
